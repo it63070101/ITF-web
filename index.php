68397fc09c54df3616a44653141bd6aac5ea2233
it63070101 <73680308+it63070101@users.noreply.github.com>
2020-10-30 11:02:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <p>ชื่อ นาย ปรัตถกร สิงห์วิเศษ(พี)</p
     <img src="https://i.pinimg.com/236x/3b/26/48/3b2648c1949f3b84c59b5e6cdf00bb00.jpg" />
     <br>
-    <img src="https://i.pinimg.com/236x/18/0b/65/180b65de6c5ae60e43a615065c8622c2.jpg">
+    <img src="https://i.pinimg.com/236x/18/0b/65/180b65de6c5ae60e43a615065c8622c2.jpg" />
     <br>
     <p>A : วันก่อนครับ</p>
     <p>B : ทำไมครับ?</p>
