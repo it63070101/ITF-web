3dd36fbc418800648fe5e61e54d65bd0df6ab1b3
it63070101 <73680308+it63070101@users.noreply.github.com>
2020-10-30 11:01:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "hello IT<br>";
-?>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>My name</title>
+  </head>
+  <body>
+    <p>ชื่อ นาย ปรัตถกร สิงห์วิเศษ(พี)</p
+    <img src="https://i.pinimg.com/236x/3b/26/48/3b2648c1949f3b84c59b5e6cdf00bb00.jpg" />
+    <br>
+    <img src="https://i.pinimg.com/236x/18/0b/65/180b65de6c5ae60e43a615065c8622c2.jpg">
+    <br>
+    <p>A : วันก่อนครับ</p>
+    <p>B : ทำไมครับ?</p>
+    <p>A : วันนี้หลวม พ่ามม!!</p>
+  </body>
+</html>
