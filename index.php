4938b88df69380a3541f9cc25c4db570e6565b10
it63070101 <73680308+it63070101@users.noreply.github.com>
2020-11-06 10:56:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>My name</title>
-  </head>
-  <body>
-    <p>ชื่อ นาย ปรัตถกร สิงห์วิเศษ(พี)</p>
-    <img
-      src="https://i.pinimg.com/236x/3b/26/48/3b2648c1949f3b84c59b5e6cdf00bb00.jpg"
-    />
-    <br />
-    <img
-      src="https://i.pinimg.com/236x/18/0b/65/180b65de6c5ae60e43a615065c8622c2.jpg"
-    />
-    <a href="https://www.facebook.com/">Favorite web Yeah!!!!</a>
-    <br />
-    <p>A : วันก่อนครับ</p>
-    <p>B : ทำไมครับ?</p>
-    <p>A : วันนี้หลวม พ่ามม!!</p>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6" style="margin: 0; padding: 0;">
+                <img src="img/creative/creative5.jpg" style="height: 900px; width: 100%;"class="img-fluid" />
+            </div>
+            <div class="col-sm-3" style="margin: 0; padding: 0;">
+                <img src="img/life/life2.jpg" style="height: 300px;" class="img-fluid" />
+                <img src="img/life/life2.jpg" style="height: 300px;" class="img-fluid" />
+                <img src="img/life/life2.jpg" style="height: 300px;" class="img-fluid" />
+            </div>
+            <div class="col-sm-3" style="margin: 0; padding: 0;">
+                <img src="img/staffs/staff1.jpg" style="height: 600px;" class="img-fluid" />
+                <img src="img/staffs/staff2.jpg" style="height: 300px;" class="img-fluid" />
+            </div>
+    </div> <!-- Close container -->
+</body>
 </html>
