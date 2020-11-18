c5ebe1b0080d6f8ea731f91210663e627d091349
it63070101 <73680308+it63070101@users.noreply.github.com>
2020-11-18 23:07:35 +0700

    Update show.php

--- show.php
+++ show.php
@@ -23,7 +23,7 @@
 						<div class="table-responsive">
 							<?php
 								$conn = mysqli_init();
-								mysqli_real_connect($conn, 'suphafang.mysql.database.azure.com', 'suphafang@suphafang', 'Fang2545', 'ITFLab', 3306);
+								mysqli_real_connect($conn, 'parattakorn.mysql.database.azure.com', 'it63070101@parattakorn', 'UNDdws73', 'itflab', 3306);
 								if (mysqli_connect_errno($conn))
 								{
 								    die('Failed to connect to MySQL: '.mysqli_connect_error());
